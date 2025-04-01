3291593fd9761767fc2be9b8e0f74f0ebefb1eca
YannicRamser <yannick.ramser@bluewin.ch>
2025-04-01 09:59:43 +0200

    Third commit

--- project-app/src/App.jsx
+++ project-app/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {BrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
 import {createBrowserRouter} from "react-router-dom";
