eee405d65d273c76a51cbc7cba52d3eee7899bed
YannicRamser <yannick.ramser@bluewin.ch>
2025-04-01 12:48:17 +0200

    Header added

---
+++ project-app/src/pages/Header.jsx
@@ -0,0 +1,23 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <>
+            <div id="header-container">
+                <h1 id="header-title">Ameti</h1>
+
+                <div id="header-icons">
+                    <div id="header-navBar">
+                        <h2 id="header-navIcon">Home</h2>
+                        <h2 id="header-navIcon">Dashboard</h2>
+                    </div>
+
+                    <img id="header-settings" src="../assets/settings-icon.png" width={40} height={40} alt="Settings"/>
+                </div>
+
+            </div>
+            <div id="header-line"/>
+        </>
+    )
+
+}
