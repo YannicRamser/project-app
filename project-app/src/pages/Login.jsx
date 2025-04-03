12c6038b11125448b2b07e9f3d2ac5e8f4492473
E1isa <145186400+E1isa@users.noreply.github.com>
2025-04-03 09:16:41 +0200

    Second commit

--- project-app/src/pages/Login.jsx
+++ project-app/src/pages/Login.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import './Login.css';
+import {useNavigate } from 'react-router-dom';
 
 const Login = () => {
+const navigate = useNavigate();
     return (
+
         <div className="login-container">
             <div className="login-form">
                 <div id="login-Title">
                     <h1>Login</h1>
+
+                    <p id="Risposta"></p>
+
                 </div>
-                <div id="login-CampoUsername" className="input-field">
+                <div id="login-Username" className="input-field">
                     <p>Username</p>
-                    <input type="text" />
+                    <input id="input-username" type="text"/>
                 </div>
                 <div id="login-Password" className="input-field">
                     <p>Password</p>
-                    <input type="password" />
+                    <input id="input-password" type="password"/>
+
                 </div>
                 <div className="login-button-container">
-                    <button>Login</button>
+                    <p id="Risposta"> </p>
+                    <button onClick={(pswd, username) => {
+
+                        username = document.querySelector("#input-username").value;
+                        pswd = document.querySelector("#input-password").value;
+                        if (pswd === "1234" && username === "username") {
+                            navigate("/");
+                        } else {
+                            let text = "Username o password sono errati";
+                            document.getElementById("Risposta").innerHTML = text;
+                        }
+                    }}>Login
+                    </button>
+
                 </div>
             </div>
         </div>
